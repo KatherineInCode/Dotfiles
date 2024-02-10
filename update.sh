a5a7c7d21cd0d081c223cc8cc8845b0ec4b5ab4b
Katherine Bertelsen <katherine@katherineincode.com>
2024-02-09 21:19:18 -0600

    Add update script

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Homebrew
+
+brew update
+brew upgrade
+brew bundle --file=~/.df/Brewfile --no-lock
+brew cleanup
+
+# Stow the actual dotfiles
+
+stow --verbose --restow --dir ~/.df/ --target ~/ --ignore=\.DS_Store dotfiles
+
+# Vim plugins
+
+vim +PluginInstall +qall
+
+# Ruby
+
+# Python
+
+pip3 install --quiet --upgrade pip
+pip3 install --quiet --user --upgrade -r ~/.df/Pipfile
