4aeac3b06f239bf4cf0a2aa5b9d370483e96d19d
Katherine Bertelsen <katherine@katherineincode.com>
2025-01-03 13:30:29 -0600

    Additional aliases and functions

--- includes/xcode.bash
+++ includes/xcode.bash
@@ -7,9 +7,20 @@ cleandd() {
 
 alias cleardd=cleandd
 
+cleansims() {
+    xcrun simctl --set previews delete all
+    echo "Deleted all cached previews."
+}
+
+alias clearsims=cleansims
+
 function openws {
     for f in ./*.xcworkspace; do
         open "${f}"
         break;
     done
 }
+
+alias sbs="Scripts/bootstrap.sh"
+alias csbs="cleardd && sbs"
+alias ccsbs="cleardd && clearsims && sbs"
